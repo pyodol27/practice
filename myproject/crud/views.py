199a9176808d677cd2d42a165b596e477d72529e
pyodol27 <pvyyo@naver.com>
2022-09-30 14:26:33 +0900

    'practice'

---
+++ myproject/crud/views.py
@@ -0,0 +1,31 @@
+from re import X
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from .forms import StudentForm
+from .models import Student
+
+
+# Create your views here.
+def add(request):
+    form = StudentForm(request.POST or None)
+    #student = Student.objects.all()
+    if form.is_valid():
+        form.save()
+    return render(request, 'add.html', {'form': form})
+
+def show(request):
+    student = Student.objects.all()
+    return render(request, 'show.html', {'student': student})
+
+def update(request, id):
+    student = Student.objects.get(id=id)
+    form = StudentForm(request.POST, instance=student)
+    if form.is_valid():
+        form.save()
+        return HttpResponseRedirect('/')
+    return render(request, 'update.html', {'student': student})
+
+def delete(request, id):
+    form = Student.objects.get(id=id)
+    form.delete()
+    return HttpResponseRedirect('/')
